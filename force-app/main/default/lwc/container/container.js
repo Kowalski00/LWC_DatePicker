de5f7d9fa1c2ccfadb41f30aca1d51d5748133a2
Ramon Kowalski <ramon.kowalski@topi.io>
2022-08-16 20:28:02 -0300

    Updates container class for tests

--- force-app/main/default/lwc/container/container.js
+++ force-app/main/default/lwc/container/container.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track  } from 'lwc';
 
-export default class Container extends LightningElement {}
+export default class Container extends LightningElement {
+
+    @track dateContext = new Date();
+    @track minDate = new Date();
+    @track dateSelected;
+
+
+    handleChangeDate(event) {
+
+        let selectedDate = event.detail;
+
+        this.dateSelected = this.formatDateISO8601(selectedDate);
+
+    }
+
+    formatDateISO8601(date){
+        return date.getFullYear()
+        +'-'+('0' + (date.getMonth() + 1)).slice(-2)
+        +'-'+('0' + date.getDate()).slice(-2);
+    }
+    
+
+}
